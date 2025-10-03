d4bfc793e7481440be29a44cb9c3fbf3a28a25d0
yaghob abdi <y.abdi@rashasoft.ir>
2025-10-03 13:20:38 +0330

    Refactor

--- generators/app/templates/_projectNameLowercase-core/model/src/main/java/_packagePath/_projectNameLowercase/core/model/jima/channel/ChannelTypeReference.java
+++ generators/app/templates/_projectNameLowercase-core/model/src/main/java/_packagePath/_projectNameLowercase/core/model/jima/channel/ChannelTypeReference.java
@@ -11,69 +11,69 @@ import ir.msob.jima.core.commons.shared.ModelType;
 
 public interface ChannelTypeReference<DTO extends Dto, C extends Criteria> extends BaseChannelTypeReference<<%= idClassName %>, User, DTO, C> {
 
-    @Override
-    default TypeReference<ChannelMessage<User, ModelType>> getChannelMessageModelTypeReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, ModelType>> getChannelMessageModelTypeReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, CriteriaMessage<String, C>>> getChannelMessageCriteriaReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, CriteriaMessage<String, C>>> getChannelMessageCriteriaReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, PageableMessage<String, C>>> getChannelMessagePageableReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, PageableMessage<String, C>>> getChannelMessagePageableReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, PageMessage<String, DTO>>> getChannelMessagePageReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, PageMessage<String, DTO>>> getChannelMessagePageReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, JsonPatchMessage<String, C>>> getChannelMessageJsonPatchReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, JsonPatchMessage<String, C>>> getChannelMessageJsonPatchReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, DtoMessage<String, DTO>>> getChannelMessageDtoReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, DtoMessage<String, DTO>>> getChannelMessageDtoReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, DtosMessage<String, DTO>>> getChannelMessageDtosReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, DtosMessage<String, DTO>>> getChannelMessageDtosReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, IdMessage<String>>> getChannelMessageIdReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, IdMessage<String>>> getChannelMessageIdReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, IdsMessage<String>>> getChannelMessageIdsReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, IdsMessage<String>>> getChannelMessageIdsReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, IdJsonPatchMessage<String>>> getChannelMessageIdJsonPatchReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, IdJsonPatchMessage<String>>> getChannelMessageIdJsonPatchReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 
-    @Override
-    default TypeReference<ChannelMessage<User, LongMessage>> getChannelMessageLongReferenceType(){
-        return new TypeReference<>() {
-        };
-    }
+  @Override
+  default TypeReference<ChannelMessage<User, LongMessage>> getChannelMessageLongReferenceType() {
+    return new TypeReference<>() {
+    };
+  }
 }
