0dd1439daa700cc1ea6b8e139bc11829c82693be
yaghob abdi <y.abdi@rashasoft.ir>
2025-10-30 09:01:39 +0330

    Refactor

--- generators/app/templates/_serviceNameWithHyphen-service/src/main/java/_packagePath/_projectNameLowercase/_serviceNameLowercase/Application.java
+++ generators/app/templates/_serviceNameWithHyphen-service/src/main/java/_packagePath/_projectNameLowercase/_serviceNameLowercase/Application.java
@@ -3,7 +3,7 @@ package <%= packagePath %>.<%= projectNameLowercase %>.<%= serviceNameLowercase 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ir.msob.jima", "<%= packagePath %>"})
+@SpringBootApplication(scanBasePackages = {"ir.msob.jima", "<%= packagePath %>.<%= projectNameLowercase %>"})
 public class Application {
 
     public static void main(String[] args) {
